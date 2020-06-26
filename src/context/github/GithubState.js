dfd666467488f8b122931799f4e54f1bab484558
Michael Li <limichael1099419@gmail.com>
2020-06-26 02:21:59 -0700

    Final

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -21,14 +21,17 @@ const GithubState = (props) => {
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+
   // Search Github Users
   const searchUsers = async (text) => {
     setLoading();
 
     const res = await axios.get(
       `https://api.github.com/search/users?q=${text}&client_id=
-      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      ${githubClientId}&client_secret=
+      ${githubClientSecret}`
     );
 
     dispatch({
@@ -43,8 +46,8 @@ const GithubState = (props) => {
 
     const res = await axios.get(
       `https://api.github.com/users/${username}?client_id=
-      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      ${githubClientId}&client_secret=
+      ${githubClientSecret}`
     );
 
     dispatch({
@@ -59,8 +62,8 @@ const GithubState = (props) => {
 
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&
-      client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&
-      client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      client_id=${githubClientId}&
+      client_secret=${githubClientSecret}`
     );
 
     dispatch({
